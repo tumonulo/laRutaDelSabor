ae6c8e5d136d004204413a2b6258ce4dce2c3320
tumonulo <juegosadrian777@gmail.com>
2025-02-04 20:23:20 +0100

    edit express.static

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 3001
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors({
   origin: (origin, callback) => {
     const acceptedOrigins = [

d67e2765814d960650789ca7b5931412633b3ea4
tumonulo <166932691+tumonulo@users.noreply.github.com>
2025-02-05 10:11:26 +0100

    sdfsd

--- controllers/search/searchUrl.js
+++ controllers/search/searchUrl.js
@@ -1,18 +1,39 @@
+// const proxy = require('express-http-proxy');
+
+// module.exports = function searchUrl(req, res, next) {
+//     const url = req.params.url
+
+//     if (!url) {
+//         return res.status(400).send('No se ha proporcionado ninguna URL.')
+//     }
+
+//     const targetUrl = decodeURIComponent(url)
+
+//     const proxyMiddleware = proxy(targetUrl, {
+//         proxyReqPathResolver: (req) => req.originalUrl,
+//         xfwd: true,
+//     })
+
+//     proxyMiddleware(req, res, next)
+// };
+
 const proxy = require('express-http-proxy');
 
 module.exports = function searchUrl(req, res, next) {
-    const url = req.params.url
+    const url = req.params.url;
 
     if (!url) {
-        return res.status(400).send('No se ha proporcionado ninguna URL.')
+        return res.status(400).send('No se ha proporcionado ninguna URL.');
     }
 
-    const targetUrl = decodeURIComponent(url)
+    // Asegúrate de que la URL sea válida
+    const targetUrl = decodeURIComponent(url); // Decodifica la URL
 
     const proxyMiddleware = proxy(targetUrl, {
-        proxyReqPathResolver: (req) => req.originalUrl,
+        proxyReqPathResolver: (req) => req.originalUrl, // Mantiene la ruta original
+        // Puedes habilitar el encabezado x-forwarded para mantener la IP original
         xfwd: true,
-    })
+    });
 
-    proxyMiddleware(req, res, next)
+    proxyMiddleware(req, res, next);
 };

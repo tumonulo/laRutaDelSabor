2a486396d2ab0a21e51eb4c8e8c75dd93cd5e370
tumonulo <166932691+tumonulo@users.noreply.github.com>
2025-02-05 14:58:34 +0100

    sdf

--- routes/apps.js
+++ routes/apps.js
@@ -1,8 +1,8 @@
 const { Router } = require('express')
 const router = Router()
 
-const appsList = require('../controllers/games/appsList.js')
-const appsApp = require('../controllers/games/appsApp.js')
+const appsList = require('../controllers/apps/appsList.js')
+const appsApp = require('../controllers/apps/appsApp.js')
 
 router.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/public/html/apps.html')

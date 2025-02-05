4e5b9d2b531bccc9af99665659b1c40bf9e538bb
tumonulo <166932691+tumonulo@users.noreply.github.com>
2025-02-05 21:29:39 +0100

    sdf

--- controllers/image/imageGenerations.js
+++ controllers/image/imageGenerations.js
@@ -4,7 +4,6 @@ module.exports = async function imageGenerations(req, res) {
     const { prompt } = req.params.prompt
     const tokenImageGenerations = process.env.TOKENIMAGEGENERATIONS
     try {
-        console.log(tokenImageGenerations)
         const response = await axios.post("https://api.rnilaweera.lk/api/v1/user/dalle", {
             prompt: prompt,
             negative_prompt: "blurry, bad quality, worst quality",
@@ -17,6 +16,6 @@ module.exports = async function imageGenerations(req, res) {
         res.json({ image: imageURL })
     } catch(error) {
         console.error(error)
-        res.json({ error: 'An error occur by creating the image' + error})
+        res.json({ error: 'An error occur by creating the image'})
     }
 }

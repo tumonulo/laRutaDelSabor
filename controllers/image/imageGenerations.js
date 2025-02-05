7874e3fc0f21ef6ef238dbe50e7316f4b4a25f5a
tumonulo <166932691+tumonulo@users.noreply.github.com>
2025-02-05 21:18:57 +0100

    nkj

--- controllers/image/imageGenerations.js
+++ controllers/image/imageGenerations.js
@@ -4,7 +4,7 @@ module.exports = async function imageGenerations(req, res) {
     const { prompt } = req.params.prompt
     const tokenImageGeberations = process.env.TOKENIMAGEGENERATIONS
     try {
-        const response = await axios.post("https://api.rnilaweera.lk/api/v1/user/dalle", {
+        const response = await axios.get("https://api.rnilaweera.lk/api/v1/user/dalle", {
             prompt: prompt,
             negative_prompt: "blurry, bad quality, worst quality",
         }, {

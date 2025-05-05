273fd70c95f03d39c45373eb0f5d24c54f86adb7
tumonulo <juegosadrian777@gmail.com>
2025-05-05 23:45:07 +0200

    agreglar todo y terminar el home

--- routes/contact.js
+++ routes/contact.js
@@ -2,7 +2,7 @@ const { Router } = require('express')
 const router = Router()
 
 router.get('/', (req, res) => {
-    res.render('pages/contacto')
+    res.render('pages/contact')
 })
 
 module.exports = router
